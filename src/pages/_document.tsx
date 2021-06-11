b61b50ef3958635affc507c066d134c7269b54f2
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-06-11 07:51:13 -0300

    feat: setup font text on project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,18 @@
-import Document from 'next/document';
+import { ReactElement } from 'react';
+import Document, { Head, Html } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+      </Html>
+    );
+  }
 }
